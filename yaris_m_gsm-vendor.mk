a078db68bc3050fa56e44e3102045977722f0d0e
kirito96 <eagle1968@live.com>
2017-02-09 11:58:36 -0400

    Initial commit

---
+++ yaris_m_gsm-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/tct/yaris_m_gsm/proprietary/vendor,system/vendor)
